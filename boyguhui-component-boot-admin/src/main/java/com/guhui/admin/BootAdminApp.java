6233a19e5d6bb73395b60478f9a4e76b8e6e0891
guhui <2075627298@qq.com>
2019-01-29 11:40:11 +0800

    新增：集成SpringBootAdmin项目（半成品,功能未实现）,修改项目中logback-spring.xml中日志存储目录错误问题

---
+++ boyguhui-component-boot-admin/src/main/java/com/guhui/admin/BootAdminApp.java
@@ -0,0 +1,76 @@
+package com.guhui.admin;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+/**
+ * Created by guhui ^-^ on 2019/1/16.
+ *
+ * @Description: java类作用描述
+ * @Author: guhui
+ * @CreateDate: 2019/1/16$ 11:57$
+ * @Version: 1.0
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableEurekaClient
+@EnableAdminServer
+@SpringBootApplication
+public class BootAdminApp {
+
+	public static void main(String[] args){
+		SpringApplication.run(BootAdminApp.class,args);
+		System.out.println("BootAdminApp服务启动完成");
+	}
+
+
+	@Profile("insecure")
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll()//
+					.and().csrf().disable();
+		}
+	}
+
+	@Profile("secure")
+	@Configuration
+	public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+		private final String adminContextPath;
+
+		public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+			this.adminContextPath = adminServerProperties.getContextPath();
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+			successHandler.setTargetUrlParameter("redirectTo");
+
+			http.authorizeRequests()
+					.antMatchers(adminContextPath + "/assets/**").permitAll()
+					.antMatchers(adminContextPath + "/login").permitAll()
+					.anyRequest().authenticated()
+					.and()
+					.formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+					.logout().logoutUrl(adminContextPath + "/logout").and()
+					.httpBasic().and()
+					.csrf().disable();
+			// @formatter:on
+		}
+	}
+
+}
